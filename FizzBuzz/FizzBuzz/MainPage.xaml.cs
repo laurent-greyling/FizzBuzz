b278793ddf270eda499cc2f09779b0ab0631706e
Laurent Greyling <l.greyling@niposoftware.com>
2018-06-29 14:17:41 +0200

    FizzBuzz Game

--- FizzBuzz/FizzBuzz/MainPage.xaml.cs
+++ FizzBuzz/FizzBuzz/MainPage.xaml.cs
@@ -1,17 +1,158 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using FizzBuzz.ViewModels;
 using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FizzBuzz
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        public static PrintNumbersViewModel Number { get; set; }
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+        private const string FizzBuzz = "FizzBuzz";
+        private bool? IsCorrect;
+
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+            Number = new PrintNumbersViewModel();
+
+            BindingContext = Number;
+        }
+
+        public void Select_Fizz(object sender, ClickedEventArgs e)
+        {
+            DisableButtons();
+            var textValue = CheckNumber(int.Parse(MyNumber.Text));
+
+            if (textValue == Fizz)
+            {
+                Success();
+            }
+            else
+            {
+                Failure(textValue);
+            }
+        }
+
+        public void Select_Buzz(object sender, ClickedEventArgs e)
+        {
+            DisableButtons();
+            var textValue = CheckNumber(int.Parse(MyNumber.Text));
+
+            if (textValue == Buzz)
+            {
+                Success();
+            }
+            else
+            {
+                Failure(textValue);
+            }
+        }
+
+        public void Select_FizzBuzz(object sender, ClickedEventArgs e)
+        {
+            DisableButtons();
+            var textValue = CheckNumber(int.Parse(MyNumber.Text));
+
+            if (textValue == FizzBuzz)
+            {
+                Success();
+            }
+            else
+            {
+                Failure(textValue);
+            }
+        }
+
+        public void Select_None(object sender, ClickedEventArgs e)
+        {
+            DisableButtons();
+            var textValue = CheckNumber(int.Parse(MyNumber.Text));
+
+            if (string.IsNullOrEmpty(textValue))
+            {
+                Success();
+            }
+            else
+            {
+                Failure(textValue);
+            }
+        }
+
+        public void Reset_Value()
+        {            
+            IsSuccess.Text = string.Empty;
+            EnableButtons();
+            if (IsCorrect != null)
+            {
+                if ((bool)IsCorrect)
+                {
+                    IsCorrect = false;
+                }
+            }
+            else
+            {
+                SetIncorrectCount();
+            }
+        }
+
+        private void DisableButtons()
+        {
+            FizzButton.IsEnabled = false;
+            BuzzButton.IsEnabled = false;
+            FizzBuzzButton.IsEnabled = false;
+            NoneButton.IsEnabled = false;
+        }
+
+        private void EnableButtons()
+        {
+            FizzButton.IsEnabled = true;
+            BuzzButton.IsEnabled = true;
+            FizzBuzzButton.IsEnabled = true;
+            NoneButton.IsEnabled = true;
+        }
+
+        private void Success()
+        {
+            IsSuccess.Text = "Correct";
+            IsSuccess.TextColor = Color.Green;
+            IsCorrect = true;
+            var correctAnswers = int.Parse(CorrectCount.Text);
+            correctAnswers++;
+            CorrectCount.Text = correctAnswers.ToString();
+        }
+
+        private void Failure(string textValue)
+        {
+            IsSuccess.Text = !string.IsNullOrEmpty(textValue) ? $"Incorrect: Should be {textValue}" : "Incorrect";
+            IsSuccess.TextColor = Color.Red;
+            IsCorrect = false;
+            SetIncorrectCount();
+        }
+
+        private void SetIncorrectCount()
+        {
+            var incorrectAnswers = string.IsNullOrEmpty(IncorrectCount.Text) ? 0 : int.Parse(IncorrectCount.Text);
+            incorrectAnswers++;
+            IncorrectCount.Text = incorrectAnswers.ToString();
+        }
+
+        private string CheckNumber(int number)
+        {
+            var fizzOrBuzz = new StringBuilder();
+
+            if (number % 3 == 0)
+            {
+                fizzOrBuzz.Append(Fizz);
+            }
+
+            if (number % 5 == 0)
+            {
+                fizzOrBuzz.Append(Buzz);
+            }
+
+            return fizzOrBuzz.ToString();
+        }
+    }
 }
---
+++ FizzBuzz/FizzBuzz/ViewModels/PrintNumbersViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace FizzBuzz.ViewModels
+{
+    public class PrintNumbersViewModel : INotifyPropertyChanged
+    {
+        public int _number { get; set; } = 1;
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (_number != value)
+                {
+                    _number = value;
+                    OnPropertyChanged("Number");
+                }
+            }
+        }
+
+        public PrintNumbersViewModel()
+        {
+            SetNumber();
+        }
+
+        private void SetNumber()
+        {
+            var timer = new Timer
+            {
+                Interval = 10000
+            };
+
+            timer.Elapsed += UpdateNumber;
+            timer.Start();
+
+            if (Number == 100)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void UpdateNumber(object sender, ElapsedEventArgs e)
+        {
+            if (Number != 100)
+            {
+                Number++;
+            }         
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
